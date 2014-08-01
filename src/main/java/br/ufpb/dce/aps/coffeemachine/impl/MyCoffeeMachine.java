f05754ac0fc3e4ad85bd2cff67adad0085836cd5
AdrianoPatricio <adryanopatricio@hotmail.com>
2014-08-01 11:47:53 -0300

    Task 1.02 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,15 +1,24 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
+	private int total;
 	private ComponentsFactory factory;
+	
 
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
 		factory.getDisplay().info("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin dime) {
+		total = dime.getValue();
+		this.factory.getDisplay().info(
+				"Total: US$ " + this.total / 100 + "." + this.total % 100);
 	}
 
 }
