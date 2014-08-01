3b6a6e716739070f8721f3ba5f2caa700f4c6213
AdrianoPatricio <adryanopatricio@hotmail.com>
2014-08-01 13:10:46 -0300

    Task 1.13 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -51,30 +51,49 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
+	public void cancelWithoutIngredients(){
+		Coin[] reverse = Coin.reverse();
+		for (Coin r : reverse) {
+			for (Coin aux : coins) {
+				if (aux == r) {
+					factory.getCashBox().release(aux);		
+				}
+			}
+		}
+		coins.clear();
+		factory.getDisplay().info("Insert coins and select a drink!");
+	}
+
 	public void select(Drink drink) {
 		factory.getCupDispenser().contains(1);
 		factory.getWaterDispenser().contains(3);	
-		factory.getCoffeePowderDispenser().contains(200);
-		
-		if(drink == drink.BLACK_SUGAR){
-			factory.getSugarDispenser().contains(200);
+		if(!this.factory.getCoffeePowderDispenser().contains(200)){
+			factory.getDisplay().warn("Out of Coffee Powder");
+			cancelWithoutIngredients();
+		} 
+
+		else {
+
+			if(drink == drink.BLACK_SUGAR){
+				factory.getSugarDispenser().contains(200);
+			}
+
+			factory.getDisplay().info("Mixing ingredients.");
+			factory.getCoffeePowderDispenser().release(200);
+			factory.getWaterDispenser().release(3);	
+
+			if(drink == this.drink.BLACK_SUGAR){
+				factory.getSugarDispenser().release(200);
+			}
+
+			factory.getDisplay().info("Releasing drink.");
+			factory.getCupDispenser().release(1);
+			factory.getDrinkDispenser().release(1);
+
+			factory.getDisplay().info("Please, take your drink.");
+			factory.getDisplay().info("Insert coins and select a drink!");
+
+			coins.clear();
 		}
-		 
-		factory.getDisplay().info("Mixing ingredients.");
-		factory.getCoffeePowderDispenser().release(200);
-		factory.getWaterDispenser().release(3);	
-		
-		if(drink == this.drink.BLACK_SUGAR){
-			factory.getSugarDispenser().release(200);
-		}
-		
-		factory.getDisplay().info("Releasing drink.");
-		factory.getCupDispenser().release(1);
-		factory.getDrinkDispenser().release(1);
-		
-		factory.getDisplay().info("Please, take your drink.");
-		factory.getDisplay().info("Insert coins and select a drink!");
-		
-		coins.clear();
 	}
 }
